6b91992a4be736c8dfaf28e6bd21709b53888602
TristanVarner <tvarner3@charlotte.edu>
2026-04-08 01:13:43 -0400

    save local version before pulling

--- tasks_data.lua
+++ tasks_data.lua
@@ -1,6 +1,6 @@
 return {
   {id=2, name="Build task input form", due_date="2026-03-27", description="Name, date, desc, priority", status="pending", priority=2},
-  {id=4, name="Add file persistence", due_date="2026-03-26", description="Save/load with storage.lua", status="pending", priority=1},
+  {id=4, name="Add file persistence", due_date="2026-03-26", description="Save/load with storage.lua", status="done", priority=1},
   {id=5, name="Laundry", due_date="2026-03-26", description="Do laundry", status="pending", priority=2},
-  {id=6, name="homework", due_date="", description="Due tmr", status="pending", priority=1},
+  {id=6, name="homework", due_date="", description="Due tmr", status="done", priority=1},
 }
